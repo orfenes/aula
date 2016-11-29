90cf8296d30ae31eafdd6e7dacec2cc09a1cfc74
Rodrigo Castro <rfocastro@sp.r7.com>
2016-11-29 15:02:30 -0200

    configurando helpers form

--- notbook/app/helpers/application_helper.rb
+++ notbook/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  ESTADOS = [
+    ["Bahia", "BA"],
+    ["São Paulo", "SP"],
+    ["Rio de Janeiro", "RJ"],
+    ["Minas Gerais", "MG"],
+    ["Espirito Santo", "ES"]
+  ]
+
+  def options_for_states(selected)
+    options_for_select(ESTADOS, selected)
+  end
+
 end
