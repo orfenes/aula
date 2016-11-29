31415fb224cf2da85ce0d8f642bc19e0033d6448
Rodrigo Castro <rfocastro@sp.r7.com>
2016-11-29 11:00:24 -0200

    formularios complexos

--- notbook/app/controllers/contacts_controller.rb
+++ notbook/app/controllers/contacts_controller.rb
@@ -15,6 +15,7 @@ class ContactsController < ApplicationController
   # GET /contacts/new
   def new
     @contact = Contact.new
+    @contact.build_andress
     options_for_select
   end
 
@@ -77,6 +78,6 @@ class ContactsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def contact_params
-    params.require(:contact).permit(:name, :email, :kind_id, :rmk)
+    params.require(:contact).permit(:name, :email, :kind_id, :rmk, andress_attributes: [:street, :city, :state])
   end
 end
--- notbook/app/models/contact.rb
+++ notbook/app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ActiveRecord::Base
   belongs_to :kind
   has_one :andress
+
+  accepts_nested_attributes_for :andress
+
 end
