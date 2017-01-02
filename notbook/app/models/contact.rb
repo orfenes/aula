0d7a748af761e27f2015e257eec891b395f34d21
Rodrigo Castro <rfocastro@sp.r7.com>
2017-01-02 13:48:37 -0200

    modificando phones

--- notbook/app/models/contact.rb
+++ notbook/app/models/contact.rb
@@ -1,7 +1,10 @@
 class Contact < ActiveRecord::Base
   belongs_to :kind
   has_one :andress
+  has_many :phones
 
   accepts_nested_attributes_for :andress
+  # accepts_nested_attributes_for :phones, reject_if :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :phones, reject_if: :all_blank, allow_destroy: true
 
 end
